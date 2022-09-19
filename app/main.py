b51b04912ca2af14e188641000af3412945965ae
tidrael <trieu@concurrently.dev>
2022-09-19 11:45:29 +0700

    add model

--- app/main.py
+++ app/main.py
@@ -1,8 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File
+from PIL import Image
+from app.clf import ImageClassifier
 
-my_fastapi_app = FastAPI()
+app = FastAPI()
 
 
-@my_fastapi_app.get("/")
+@app.get("/")
 async def root():
-    return {"Nginx": "I'm alive"}
+    return {"Sever": "Running"}
+
+
+@app.post("/predict")
+async def predict(file: bytes = File(...)):
+    upload_file = file
+    image = Image.open(upload_file)
+    model = ImageClassifier()
+    prediction = model.predict(image)
+    return {f"The result is: {prediction}"}
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 sudo apt-get update
-sudo snap install docker 
+sudo snap install docker
+sudo docker build -t fastapi .
+sudo docker run -itd -p 80:80/tcp fastapi
